bf4f0277ef85c3b423f087addc68313a4b8e31d0
arbura <arbura@ymail.com>
2014-06-13 18:39:30 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -24,7 +24,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
 # set repo
-wget -O /etc/apt/sources.list "https://raw.github.com/arbura/auto/debian7/sources.list.debian7"
+wget -O /etc/apt/sources.list "https://raw.github.com/arbura/auto/master/sources.list.debian7"
 wget "http://www.dotdeb.org/dotdeb.gpg"
 wget "http://www.webmin.com/jcameron-key.asc"
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
@@ -60,7 +60,7 @@ service vnstat restart
 
 # install screenfetch
 cd
-wget 'https://raw.github.com/arbura/auto/debian7/screeftech-dev'
+wget 'https://raw.github.com/arbura/auto/master/screeftech-dev'
 mv screeftech-dev /usr/bin/screenfetch
 chmod +x /usr/bin/screenfetch
 echo "clear" >> .profile
@@ -70,24 +70,24 @@ echo "screenfetch" >> .profile
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.github.com/arbura/auto/debian7/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.github.com/arbura/auto/master/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Modified by Yurissh OpenSource</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/arbura/auto/debian7/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/arbura/auto/master/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
 service nginx restart
 
 # install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arbura/auto/debian7/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arbura/auto/master/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/arbura/auto/debian7/1194.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/arbura/auto/master/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://raw.github.com/arbura/auto/debian7/iptables.up.rules"
+wget -O /etc/iptables.up.rules "https://raw.github.com/arbura/auto/master/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 sed -i $MYIP2 /etc/iptables.up.rules;
 iptables-restore < /etc/iptables.up.rules
@@ -95,7 +95,7 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arbura/auto/debian7/1194-client.conf"
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arbura/auto/master/1194-client.conf"
 sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
 useradd -M -s /bin/false YurisshOS
@@ -107,9 +107,9 @@ cp client.tar /home/vps/public_html/
 cd
 
 # install badvpn
-wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arbura/auto/debian7/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arbura/auto/master/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arbura/auto/debian7/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arbura/auto/master/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
@@ -182,7 +182,7 @@ apt-get -y install fail2ban;service fail2ban restart
 
 # install squid3
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.github.com/arbura/auto/debian7/squid3.conf"
+wget -O /etc/squid3/squid.conf "https://raw.github.com/arbura/auto/master/squid3.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
 
@@ -198,15 +198,15 @@ service vnstat restart
 # download script
 cd
 wget -O speedtest_cli.py "https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py"
-wget -O bench-network.sh "https://raw.github.com/arbura/auto/debian7/bench-network.sh"
+wget -O bench-network.sh "https://raw.github.com/arbura/auto/master/bench-network.sh"
 wget -O ps_mem.py "https://raw.github.com/pixelb/ps_mem/master/ps_mem.py"
-wget -O dropmon "https://raw.github.com/arbura/auto/debian7/dropmon.sh"
-wget -O userlogin.sh "https://raw.github.com/arbura/auto/debian7/userlogin.sh"
-wget -O userexpired.sh "https://raw.github.com/arbura/auto/debian7/userexpired.sh"
-wget -O userlimit.sh "https://raw.github.com/arbura/auto/debian7/userlimit.sh"
-wget -O expire.sh "https://raw.github.com/arbura/auto/debian7/expire.sh"
-#wget -O autokill.sh "https://raw.github.com/arbura/auto/debian7/autokill.sh"
-wget -O /etc/issue.net "https://raw.github.com/arbura/auto/debian7/banner"
+wget -O dropmon "https://raw.github.com/arbura/auto/master/dropmon.sh"
+wget -O userlogin.sh "https://raw.github.com/arbura/auto/master/userlogin.sh"
+wget -O userexpired.sh "https://raw.github.com/arbura/auto/master/userexpired.sh"
+wget -O userlimit.sh "https://raw.github.com/arbura/auto/master/userlimit.sh"
+wget -O expire.sh "https://raw.github.com/arbura/auto/master/expire.sh"
+#wget -O autokill.sh "https://raw.github.com/arbura/auto/master/autokill.sh"
+wget -O /etc/issue.net "https://raw.github.com/arbura/auto/master/banner"
 echo "@reboot root /root/userexpired.sh" > /etc/cron.d/userexpired
 echo "@reboot root /root/userlimit.sh" > /etc/cron.d/userlimit
 echo "0 */6 * * * root /sbin/reboot" > /etc/cron.d/reboot
