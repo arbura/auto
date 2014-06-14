39a91025d85e8cbedadcc5ce38a608799c82e44e
arbura <arbura@ymail.com>
2014-06-15 01:49:12 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -294,3 +294,5 @@ echo "VPS AUTO REBOOT TIAP 6 JAM"  | tee -a log-install.txt
 echo "SILAHKAN REBOOT VPS ANDA"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "==============================================="  | tee -a log-install.txt
+
+sudo dpkg-reconfigure dash
